b5030a9e8ab2c06c6ba0e887bf46ce6afe37193a
Ccolina03 <colinaperu2003@gmail.com>
2023-07-02 23:41:50 -0600

    Adding structure for Header and banner

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+    return (
+        <header className="w-full mx-auto">
+            <div>
+                Navigation
+            </div>
+            <div>
+                About me
+            </div>
+            <div>
+                Button for something
+            </div>
+        </header>
+    )
+}
