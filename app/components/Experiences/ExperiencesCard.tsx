6d21b3d5957a0f717ab9783fe653ff2931903848
Ccolina03 <colinaperu2003@gmail.com>
2023-07-02 22:53:40 -0600

    Implemented the Project section in Next.js 13

--- app/components/Experiences/ExperiencesCard.tsx
+++ app/components/Experiences/ExperiencesCard.tsx
@@ -1,5 +1,5 @@
-'use-client'
-import React, { FC } from 'react';
+"use client"
+import * as React from "react";
 import Heading from '../Heading';
 import Image from 'next/image';
 import '../../../app/globals.css'
@@ -23,12 +23,13 @@ const getInverse = (inverse: boolean, screenSize: 'xs' | 'sm' | 'md' | 'lg' | 'x
     case 'xl':
       return inverse ? 'jobinverse px-24 py-8' : 'job1 px-10 py-8';
     case 'sm':
+    case 'xs':
     default:
       return 'job1 px-10 py-8';
   }
 };
 
-const ExperienceCard: FC<ExperienceCardProps> = ({
+const ExperienceCard: React.FC<ExperienceCardProps> = ({
   inverse,
   job = 'Dream Job',
   company = 'Shopify',
@@ -36,10 +37,33 @@ const ExperienceCard: FC<ExperienceCardProps> = ({
   bullet2 = 'Front-end tasksfdsssssssssssssssssssssssssss',
   bullet3 = 'front-end more tasks to dofsdaaaaaaaaaaaaa',
 }) => {
+  const [screenSize, setScreenSize] = React.useState<'xs' | 'sm' | 'md' | 'lg' | 'xl'>('lg');
+  const inverseCard = getInverse(inverse || false, screenSize);
+  React.useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth >= 1280) {
+        setScreenSize('xl');
+      } else if (screenWidth >= 1024) {
+        setScreenSize('lg');
+      } else if (screenWidth >= 768) {
+        setScreenSize('md');
+      } else if (screenWidth >= 640) {
+        setScreenSize('sm');
+      } else {
+        setScreenSize('xs');
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <div className="circle1 relative overflow-visible flex justify-center items-center rounded-full border-2 border-white h-28 w-full hover:bg-green">
      <Image src={UAlberta} width={50} height={65} alt='Icon' />
-      <div className={`job1 absolute rounded-[25px]`}>
+      <div className={`${inverseCard} px-10 py-8 absolute rounded-[25px]`}>
         <Heading className="text-white font-extrabold" size="h3">
           {job}
         </Heading>
