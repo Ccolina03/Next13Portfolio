ed8334725c4172482a7b3e9c4de5db9ff1313014
Ccolina03 <colinaperu2003@gmail.com>
2023-07-03 01:06:13 -0600

    Fixed sidebar responsiveness and implemented structure for about me section

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,17 +1,45 @@
-
+import Image from "next/image";
+import CCIcon from '../../../public/cc.xlarge.png'
+import SideBarIcon from "../shared/SideBarIcon";
+import {AiOutlineGithub} from 'react-icons/ai'
+import { BsLinkedin } from "react-icons/bs";
+import {MdWork} from 'react-icons/md'
+import {BiSolidHappyAlt} from 'react-icons/bi'
+import {DiCodeigniter} from 'react-icons/di'
+import {FaLaptopCode} from 'react-icons/fa'
 
 const Header = () => {
     return (
         <header className="w-full mx-auto">
-            <div>
-                Navigation
+            <div className="relative gap-x-8 flex-row flex">
+                <div className="my-auto mx-2">
+                    <Image src={CCIcon} alt="Carlos Colina Icon" height={40} width={50}/>
+                </div>
+                <div className="flex mt-1  text-white">
+                    <a className="mx-4 my-auto"> <SideBarIcon icon={<BiSolidHappyAlt size = "40"/>}/> </a>
+                    <a className="mx-6 my-auto"><SideBarIcon icon={<MdWork size = "40"/>}/></a>
+                    <a className="mx-6 my-auto"> <SideBarIcon icon={<FaLaptopCode size = "40"/>}/> </a>
+                    <a className="mx-6 my-auto"> <SideBarIcon icon={<DiCodeigniter size = "40"/>}/> </a>
+                </div>
+                <div className="absolute gap-x-5 mt-1 flex top-0 right-5 ">
+                    <a className="my-auto"> <SideBarIcon icon={<AiOutlineGithub size = "40"/>}/></a>
+                    <a className="my-auto "><SideBarIcon icon={<BsLinkedin size = "40"/>}/></a>
+                </div>
             </div>
-            <div>
-                About me
+            <div className="flex text-white">
+                <div className="flex-col">
+                    <div className="nombre">nombre</div>
+                    <div className="subtitulo1">sub1</div>
+                </div>
+                <div className="flex">
+                    <Image/>
+                </div>
             </div>
-            <div>
+            <div className="text-white">
                 Button for something
             </div>
         </header>
     )
 }
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import Experiences from './components/Experiences/Experiences'
 import Projects from './components/Projects/Projects'
+import Header from './components/Header/Header'
+
 export default function Home() {
   return (
     <main className='flex flex-col gap-y-16  w-full space-y-10 bg-backgroundWork'>
+      <Header/>
       <Experiences/>
       <Projects/>
     </main>
