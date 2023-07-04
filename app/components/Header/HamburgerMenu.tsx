e1ddcb5d2d3a523617bc6261fa69ce42125c06b3
Ccolina03 <colinaperu2003@gmail.com>
2023-07-04 01:00:46 -0600

    Added Hamburger menu unclick elsewhere

--- app/components/Header/HamburgerMenu.tsx
+++ app/components/Header/HamburgerMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import * as React from 'react';
 import { AiOutlineGithub } from 'react-icons/ai';
 import { BsLinkedin } from 'react-icons/bs';
 import { AiOutlineMenu } from 'react-icons/ai';
@@ -13,23 +13,37 @@ interface HamburgerMenuProps {
 }
 
 const HamburgerMenu: React.FC<HamburgerMenuProps> = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const menuRef = React.useRef<HTMLDivElement>(null);
 
   const toggleMenu = (): void => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="absolute flex flex-row justify-between items-start top-3 right-2 xs:block">
+    <div className="absolute flex flex-row justify-between items-start top-3 right-5 xs:block">
             <div className="x-4 py-2">
     <AiOutlineMenu
             className="text-green cursor-pointer"
-             size={30}
+             size={50}
              onClick={toggleMenu}
            />
         </div>
       {isMenuOpen && (
-        <div className="bg-backgroundWork absolute top-0 right-0 flex flex-row-reverse">
+        <div ref={menuRef} className="bg-backgroundWork absolute top-0 right-0 flex flex-row-reverse">
           <a href='https://www.linkedin.com/in/ccolina03/' className="mx-4 my-auto"> <SideBarIcon icon={<BsLinkedin size = "40"/>}/> </a>
           <a href='#Carlos' className="mx-4 my-auto"> <SideBarIcon icon={<AiOutlineGithub size = "40"/>}/> </a>
           <a href='#Carlos' className="mx-4 my-auto"> <SideBarIcon icon={<BiSolidHappyAlt size = "40"/>}/> </a>
