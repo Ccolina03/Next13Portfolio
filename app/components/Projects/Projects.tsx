fd60172f36c2a2effa4e22f780fadeb53c965785
Ccolina03 <colinaperu2003@gmail.com>
2023-07-14 14:27:55 -0600

    Adding UPrinti Link

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
     <div id="projects" className='projects-container gap-16 flex flex-col'>
         <Heading className="text-4xl self-center text-white" size='h1'>My Projects</Heading>
         <div className='grid sm:grid-cols-1 md:grid-cols-2'>
-            <Card picture={UPrintI} title='UPrintI (Next.js-GraphQL-TailwindCSS)' subtitle="Mom's Online Toy Store" hrefGH='https://github.com/Ccolina03/UprintI'/>
+            <Card picture={UPrintI} title='UPrintI (Next.js-GraphQL-TailwindCSS)' subtitle="Mom's Online Toy Store" hrefGH='https://github.com/Ccolina03/UprintI' hrefYT='https://youtu.be/TVz4vjbxoVA'/>
             <Card picture={BajaBaja} title='BajaBaja (React-Node.js-MongoDB-Express)' subtitle="Lima's true/false informal bus stops tracker" hrefGH='https://github.com/Ccolina03/BajaBajaBackEnd_v1' hrefYT='https://youtu.be/SgcVEeWLTMw'/>
             <Card picture={Go} title='SinFin (React-Golang-AWS CDK)' subtitle="Future PWA Idea" hrefGH='https://github.com/Ccolina03' hrefYT='https://www.onlygfx.com/wp-content/uploads/2018/04/4-coming-soon-stamp-cover.png'/>
             <Card picture={Challenge} title='Challenge (Redux-JavaScript)' subtitle="Redux app integrated with API + Search filter" hrefGH='https://github.com/Ccolina03/carlos_colina_launchpad-react-code-challenge'hrefYT='https://youtu.be/-xbvDp44gTY'/>
