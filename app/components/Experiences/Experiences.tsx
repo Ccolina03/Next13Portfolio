d530ae9f0db9c69fd125b8b21e53d3fe81666a3e
Ccolina03 <colinaperu2003@gmail.com>
2023-08-11 19:21:48 -0600

    Fixing content for Experiences

--- app/components/Experiences/Experiences.tsx
+++ app/components/Experiences/Experiences.tsx
@@ -9,12 +9,13 @@ const Experiences = () => {
                 <div className='timeline w-28 bg-backgroundWork flex flex-col items-center'>
                     <div className='line1 w-0.5 bg-gray-100 h-[12rem]'></div>
                     <ExperienceCard job='SWE Intern' company='Litespace' date='May - August 2023'
-                    bullet1='Built 9+ reusables components utilizing TypeScript and React into Storybook and a Progressive Web App '
-                    bullet2='Implemented Nest.js backend migration for domain-specific microservice architecture using GraphQL, RabbitMQ, Docker, and EKS'/>
+                    bullet1='Built 8+ reusables components utilizing TypeScript and React into Storybook and a Progressive Web App '
+                    bullet2='Developed detailed design documentation for the migration of microservice domains, based on Nest.js, GraphQL,
+                    AWS and MongoDB Atlas'/>
                     <div className='line1 w-0.5 bg-gray-100 xl:h-96 h-96 xs:h-[39rem] md:h-72 lg:h-72 sm:h-96' ></div>
                     <div className='circle1 flex items-center justify-center rounded-full border-2 border-white h-28 w-full hover:bg-green'>
-                    <ExperienceCard inverse={true} job='Digital Content QA' company='University of Alberta' date = "June - August 2023" bullet1='Converted 15+ reusables components to TypeScript and implemented them lively to 90000+ pages'
-                    bullet2='Refactored HTML and CSS code of more than 2000+ web pages, while improving Accesibility and SEO' />
+                    <ExperienceCard inverse={true} job='Digital Content QA' company='University of Alberta' date = "June - August 2023" bullet1='Converted JavaScript modules to TypeScript, leveraging jQuery and AJAX for improved functionality and UX.'
+                    bullet2='Refactored HTML and CSS code of more than 800+ web pages, while improving Accesibility and SEO' />
                     </div>
                     <div className='line1 w-0.5 bg-gray-100 h-96 xl:h-96 xs:h-[39rem] md:h-72 sm:h-96'></div>
                     <div className='circle1 flex justify-center items-center rounded-full border-2 border-white h-28 w-full hover:bg-green'>
