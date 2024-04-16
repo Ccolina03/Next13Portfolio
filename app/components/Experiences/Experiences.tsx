5b0331a861e23ede562836566a5c4c4d5b7d4e22
Carlos Colina <107608945+Ccolina03@users.noreply.github.com>
2024-04-16 17:18:22 -0600

    Update Experiences.tsx

--- app/components/Experiences/Experiences.tsx
+++ app/components/Experiences/Experiences.tsx
@@ -14,14 +14,14 @@ const Experiences = () => {
                     bullet3='Developing a prototype project to synchronize computed spatial data between PostgreSQL and ElasticSearch, automating dashboard updates and enabling side-by-side views'/>
                     <div className='line1 w-0.5 bg-gray-100 xl:h-96 h-96 xs:h-[39rem] md:h-72 lg:h-72 sm:h-96' ></div>
                     <div className='circle1 flex items-center justify-center rounded-full border-2 border-white h-28 w-full hover:bg-green'>
-                         <ExperienceCard job='SWE Intern' company='Litespace' date='May - August 2023'
+                         <ExperienceCard inverse={true} job='SWE Intern' company='Litespace' date='May - August 2023'
                     bullet1='Built 8+ reusables components utilizing TypeScript and React into Storybook and a Progressive Web App '
                     bullet2='Developed detailed design documentation for the migration of microservice domains, based on Nest.js, GraphQL,
                     AWS and MongoDB Atlas'/>
                     </div>
                     <div className='line1 w-0.5 bg-gray-100 h-96 xl:h-96 xs:h-[39rem] md:h-72 sm:h-96'></div>
                     <div className='circle1 flex justify-center items-center rounded-full border-2 border-white h-28 w-full hover:bg-green'>
-                     <ExperienceCard inverse={true} job='Digital Content QA' company='University of Alberta' date = "June - August 2023" bullet1='Converted JavaScript modules to TypeScript, leveraging jQuery and AJAX for improved functionality and UX.'
+                     <ExperienceCard job='Digital Content QA' company='University of Alberta' date = "June - August 2023" bullet1='Converted JavaScript modules to TypeScript, leveraging jQuery and AJAX for improved functionality and UX.'
                     bullet2='Refactored HTML and CSS code of more than 800+ web pages, while improving Accesibility and SEO' />
                     </div>
                     <div className='line1 w-0.5 bg-gray-100 h-[12rem]'></div>
